056c0701ac2274f4a79ca8ad692f11ca6c7eb2cd
Harald Kirsch <pifpafpuf@gmx.de>
2019-07-28 17:16:37 +0200

    formatting

--- tests/src/monq/jfa/PerformanceTest.java
+++ tests/src/monq/jfa/PerformanceTest.java
@@ -35,138 +35,138 @@ public class PerformanceTest {
   private CharStatistics[] charStats;
   @Before
   public void setup() throws Exception {
-      charStats = readBiStats("resources/char-2stat-english.txt.gz");
-      if (null!=System.getProperty("cobertura.active")) {
-        speedupFactor = 1.01;
-      } else {
-        speedupFactor = 1.2;
-      }
+    charStats = readBiStats("resources/char-2stat-english.txt.gz");
+    if (null!=System.getProperty("cobertura.active")) {
+      speedupFactor = 1.01;
+    } else {
+      speedupFactor = 1.2;
     }
+  }
 
-    @Test
-    public void performanceTest() throws Exception {
-      StringBuilder text = createText(20_000_000);
-      CountWords cw = new CountWords();
-      Nfa nfa = createNfa(cw);
-
-      for (int i=0; i<4; i++) {
-        Timing slow = runFilter(nfa, text, 1.0f);
-        Map<String,Count> slowMap = cw.reset();
-
-        Timing fast = runFilter(nfa, text, 100000.0f);
-        Map<String,Count> fastMap = cw.reset();
-
-        System.out.printf("slow and fast: %s>%s, speedup=%.1f%n", slow, fast,
-                          slow.speedUpOver(fast));
-        //System.out.println(slowMap);
-        assertTrue("slow and fast: "+slow+">"+fast,
-                   slow.dtSeconds()>fast.dtSeconds());
-        assertEquals(slowMap, fastMap);
-        assertTrue(slow.speedUpOver(fast)>speedupFactor);
-      }
+  @Test
+  public void performanceTest() throws Exception {
+    StringBuilder text = createText(20_000_000);
+    CountWords cw = new CountWords();
+    Nfa nfa = createNfa(cw);
+    
+    for (int i=0; i<4; i++) {
+      Timing slow = runFilter(nfa, text, 1.0f);
+      Map<String,Count> slowMap = cw.reset();
+      
+      Timing fast = runFilter(nfa, text, 100000.0f);
+      Map<String,Count> fastMap = cw.reset();
+      
+      System.out.printf("slow and fast: %s>%s, speedup=%.1f%n", slow, fast,
+                        slow.speedUpOver(fast));
+      //System.out.println(slowMap);
+      assertTrue("slow and fast: "+slow+">"+fast,
+                 slow.dtSeconds()>fast.dtSeconds());
+      assertEquals(slowMap, fastMap);
+      assertTrue(slow.speedUpOver(fast)>speedupFactor);
     }
-
-    @Test
-    public void compareToRegexTest() throws Exception {
-      // we have this test here, because everything is there to create random
-      // text.
-      final Map<Object,Object> counts = new HashMap<>();
-      MapProvider mp = new MapProvider() {
-        @Override
-        public Map<Object,Object> getMap() {
-          return counts;
+  }
+  
+  @Test
+  public void compareToRegexTest() throws Exception {
+    // we have this test here, because everything is there to create random
+    // text.
+    final Map<Object,Object> counts = new HashMap<>();
+    MapProvider mp = new MapProvider() {
+      @Override
+      public Map<Object,Object> getMap() {
+        return counts;
+      }
+    };
+    String[] rexes = {"<[^>]*>", "[a-zA-Z]+", "[0-9]+", "[ \r\n\t]+"};
+    String[] names = {"xml", "alpha", "num", "space"};
+    
+    Nfa nfa = new Nfa(Nfa.NOTHING);
+    for (int i=0; i<rexes.length; i++) {
+      nfa.or(rexes[i], new CountAction(names[i]));
+    }
+    
+    StringBuilder text = createText(200_000);
+    DfaRun r = new DfaRun(nfa.compile(DfaRun.UNMATCHED_DROP));
+    r.setIn(new CharSequenceCharSource(text));
+    r.clientData = mp;
+    r.filter();
+    System.out.println(counts);
+    Map<String,Count> javaResult = countMatches(rexes, names, text);
+    System.out.println(javaResult);
+    assertEquals(javaResult, counts);
+  }
+  
+  private static Map<String,Count>
+  countMatches(String[] rexes, String[] names, StringBuilder text) {
+    Map<String,Count> result = new HashMap<>();
+    for (String name : names) {
+      result.put(name, new Count());
+    }
+    StringBuilder re = new StringBuilder();
+    re.append('(');
+    for (String s : rexes) {
+      re.append(s).append(")|(");
+    }
+    re.setLength(re.length()-2);
+    Pattern p = Pattern.compile(re.toString());
+    Matcher m = p.matcher(text);
+    
+    int start = 0;
+    while (m.find(start)) {
+      for(int i=0; i<rexes.length; i++) {
+        if (m.group(i+1)!=null) {
+          result.get(names[i]).count+=1;
         }
-      };
-      String[] rexes = {"<[^>]*>", "[a-zA-Z]+", "[0-9]+", "[ \r\n\t]+"};
-      String[] names = {"xml", "alpha", "num", "space"};
-
-      Nfa nfa = new Nfa(Nfa.NOTHING);
-      for (int i=0; i<rexes.length; i++) {
-        nfa.or(rexes[i], new CountAction(names[i]));
       }
-
-      StringBuilder text = createText(200_000);
-      DfaRun r = new DfaRun(nfa.compile(DfaRun.UNMATCHED_DROP));
-      r.setIn(new CharSequenceCharSource(text));
-      r.clientData = mp;
-      r.filter();
-      System.out.println(counts);
-      Map<String,Count> javaResult = countMatches(rexes, names, text);
-      System.out.println(javaResult);
-      assertEquals(javaResult, counts);
+      start = m.end();
     }
-
-    private static Map<String,Count>
-    countMatches(String[] rexes, String[] names, StringBuilder text) {
-      Map<String,Count> result = new HashMap<>();
-      for (String name : names) {
-        result.put(name, new Count());
+    return result;
+  }
+  
+  private static Timing runFilter(Nfa nfa, CharSequence text, float tradeOff)
+      throws CompileDfaException, IOException
+  {
+    nfa.setMemoryForSpeedTradeFactor(tradeOff);
+    Intervals.resetStats();
+    Dfa dfa = nfa.compile(DfaRun.UNMATCHED_DROP);
+    System.out.println(Intervals.logStats());
+    
+    DfaRun r = new DfaRun(dfa);
+    r.setIn(new CharSequenceCharSource(text));
+    Timing t = new Timing();
+    r.filter();
+    t.stop();
+    return t;
+  }
+  
+  private Nfa createNfa(CountWords cw) throws Exception {
+    String[] words = new String(createText(700)).split("[ ]+");
+    Nfa nfa = new Nfa();
+    for (String word : words) {
+      if (word.length()<4) {
+        continue;
       }
-      StringBuilder re = new StringBuilder();
-      re.append('(');
-      for (String s : rexes) {
-        re.append(s).append(")|(");
-      }
-      re.setLength(re.length()-2);
-      Pattern p = Pattern.compile(re.toString());
-      Matcher m = p.matcher(text);
-
-      int start = 0;
-      while (m.find(start)) {
-        for(int i=0; i<rexes.length; i++) {
-          if (m.group(i+1)!=null) {
-            result.get(names[i]).count+=1;
-          }
-        }
-        start = m.end();
-      }
-      return result;
+      //System.out.println("word=`"+word+"'");
+      nfa.or(nfa.escape(word), cw);
     }
-
-    private static Timing runFilter(Nfa nfa, CharSequence text, float tradeOff)
-        throws CompileDfaException, IOException
-    {
-      nfa.setMemoryForSpeedTradeFactor(tradeOff);
-      Intervals.resetStats();
-      Dfa dfa = nfa.compile(DfaRun.UNMATCHED_DROP);
-      System.out.println(Intervals.logStats());
-
-      DfaRun r = new DfaRun(dfa);
-      r.setIn(new CharSequenceCharSource(text));
-      Timing t = new Timing();
-      r.filter();
-      t.stop();
-      return t;
+    return nfa;
+  }
+  
+  /**
+   * create a random text based on the charStats values.
+   */
+  private StringBuilder createText(int nChars) {
+    StringBuilder text = new StringBuilder(nChars);
+    char lastCh = 'e';   //arbitrary first character
+    for (int i=0; i<nChars; i++) {
+      CharStatistics cs = charStats[lastCh];
+      char nextCh = cs.getRandomChar();
+      text.append(nextCh);
+      lastCh = nextCh;
     }
-
-    private Nfa createNfa(CountWords cw) throws Exception {
-      String[] words = new String(createText(700)).split("[ ]+");
-      Nfa nfa = new Nfa();
-      for (String word : words) {
-        if (word.length()<4) {
-          continue;
-        }
-        //System.out.println("word=`"+word+"'");
-        nfa.or(nfa.escape(word), cw);
-      }
-      return nfa;
-    }
-
-    /**
-     * create a random text based on the charStats values.
-     */
-    private StringBuilder createText(int nChars) {
-      StringBuilder text = new StringBuilder(nChars);
-      char lastCh = 'e';   //arbitrary first character
-      for (int i=0; i<nChars; i++) {
-        CharStatistics cs = charStats[lastCh];
-        char nextCh = cs.getRandomChar();
-        text.append(nextCh);
-        lastCh = nextCh;
-      }
-      return text;
-    }
-    /*+******************************************************************/
+    return text;
+  }
+  /*+******************************************************************/
   /**
    * reads a file with lines like
    *
@@ -209,7 +209,7 @@ public class PerformanceTest {
     }
     /*+******************************************************************/
     private static final class Timing {
-      private long start = System.currentTimeMillis();
+      private final long start = System.currentTimeMillis();
       private long stop = -1;
       public void stop() {
         stop = System.currentTimeMillis();
@@ -293,8 +293,8 @@ public class PerformanceTest {
     private static final class CharStatistics {
       private final static Random r = new Random(15121962);
 
-      private ArrayList<Long> accumulatedFreq = new ArrayList<>();
-      private ArrayList<Character> chars = new ArrayList<Character>();
+      private final ArrayList<Long> accumulatedFreq = new ArrayList<>();
+      private final ArrayList<Character> chars = new ArrayList<>();
 
       public void add(char ch, long accFrq) {
         accumulatedFreq.add(accFrq);
